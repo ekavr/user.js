85a0e0849e6bb709c24cc04159f7529035bcf75a
Evi Kavrochorianou <15310965+ekavr@users.noreply.github.com>
2021-02-01 16:50:23 +0200

    Update user.js
    experimental

--- user.js
+++ user.js
@@ -8,15 +8,6 @@
  * SECTION: HTML5 / APIs / DOM                                                *
  ******************************************************************************/
 
-// PREF: Disable Service Workers
-// https://developer.mozilla.org/en-US/docs/Web/API/Worker
-// https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorker_API
-// https://wiki.mozilla.org/Firefox/Push_Notifications#Service_Workers
-// NOTICE: Disabling ServiceWorkers breaks functionality on some sites (Google Street View...)
-// Unknown security implications
-// CVE-2016-5259, CVE-2016-2812, CVE-2016-1949, CVE-2016-5287 (fixed)
-user_pref("dom.serviceWorkers.enabled",				false);
-
 // PREF: Disable web notifications
 // https://support.mozilla.org/en-US/questions/1140439
 user_pref("dom.webnotifications.enabled",			false);
@@ -25,16 +16,6 @@ user_pref("dom.webnotifications.enabled",			false);
 // https://wiki.mozilla.org/Security/Reviews/Firefox/NavigationTimingAPI
 // https://www.w3.org/TR/navigation-timing/#privacy
 user_pref("dom.enable_performance",				false);
-
-// PREF: Make sure the User Timing API does not provide a new high resolution timestamp
-// https://trac.torproject.org/projects/tor/ticket/16336
-// https://www.w3.org/TR/2013/REC-user-timing-20131212/#privacy-security
-user_pref("dom.enable_user_timing",				false);
-
-// PREF: Disable Web Audio API
-// https://bugzilla.mozilla.org/show_bug.cgi?id=1288359
-// NOTICE: Web Audio API is required for Unity web player/games
-user_pref("dom.webaudio.enabled",				false);
 
 // PREF: Disable Location-Aware Browsing (geolocation)
 // https://www.mozilla.org/en-US/firefox/geolocation/
@@ -53,11 +34,6 @@ user_pref("geo.wifi.logging.enabled", false);
 // https://developer.mozilla.org/docs/Mozilla/B2G_OS/API/TCPSocket
 user_pref("dom.mozTCPSocket.enabled",				false);
 
-// PREF: Disable DOM storage (disabled)
-// http://kb.mozillazine.org/Dom.storage.enabled
-// https://html.spec.whatwg.org/multipage/webstorage.html
-// NOTICE-DISABLED: Disabling DOM storage is known to cause`TypeError: localStorage is null` errors
-//user_pref("dom.storage.enabled",		false);
 
 // PREF: Disable leaking network/browser connection information via Javascript
 // Network Information API provides general information about the system's connection type (WiFi, cellular, etc.)
